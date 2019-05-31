d93cf2e850d3f3ad1dce78d6d869abb75a325fd1
mac air <samal.zhex@gmail.com>
2019-05-31 21:58:57 +0600

    Получение данных из форм

--- add.php
+++ add.php
@@ -23,7 +23,7 @@ $categories = mysqli_fetch_all($result, MYSQLI_ASSOC);
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $lot = $_POST;
-    $required = ['title', 'description'];
+    $required = ['lot-name', 'message'];
     $errors = [];
     foreach ($required as $key) {
         if (empty($_POST[$key])) {
@@ -35,41 +35,43 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         $errors['category'] = 'Выберите категорию';
     }
     
-    if (isset($_FILES['lot_img']['name'])) {
-        $tmp_name = $_FILES['lot_img']['tmp_name'];
-        $path = $_FILES['lot_img']['name'];
+    if (isset($_FILES['lot-img'])) {
+        $file_name = $_FILES['lot-img']['name'];
+        $tmp_name = $_FILES['lot-img']['tmp_name'];
+        $file_path = __DIR__ . '/uploads/';
+        $file_url = '/uploads/' . $file_name;
         $finfo = finfo_open(FILEINFO_MIME_TYPE);
         $file_type = finfo_file($finfo, $tmp_name);
-        if ($file_type !== 'image/png' || $file_type !== 'image/jpeg') {
-            $errors['picture'] = 'Загрузите картинку в формате PNG, JPEG или JPG';
+        if ($file_type !== 'image/jpeg') {
+            $errors['lot-img'] = 'Загрузите картинку в формате PNG, JPEG или JPG';
         } else {
-            $lot['picture'] = $path;
+            $lot['lot-img'] = $file_url;
         }
     } else {
-        $errors['picture'] = 'Вы не загрузили файл';
+        $errors['lot-img'] = 'Вы не загрузили файл';
     }
-    if (isset($lot['price'])) {
-        if ($lot['price'] <= 0) {
-            $errors['price'] = 'Содержимое поля «начальная цена» должно быть числом больше нуля';
+    if (isset($lot['lot-rate'])) {
+        if ($lot['lot-rate'] <= 0) {
+            $errors['lot-rate'] = 'Содержимое поля «начальная цена» должно быть числом больше нуля';
         }
     } else {
-        $errors['price'] = 'Это поле надо заполнить';
+        $errors['lot-rate'] = 'Это поле надо заполнить';
     }
 
-    if (isset($lot['dt_end'])){
-        if (!is_date_valid($lot['dt_end'])) {
-            $errors['dt_end'] = 'Указанная дата должна быть больше текущей даты, хотя бы на один день и в формате «ГГГГ-ММ-ДД»';
+    if (isset($lot['lot-date'])){
+        if (!is_date_valid($lot['lot-date'])) {
+            $errors['lot-date'] = 'Указанная дата должна быть больше текущей даты, хотя бы на один день и в формате «ГГГГ-ММ-ДД»';
         }
     } else {
-        $errors['dt_end'] = 'Это поле надо заполнить';
+        $errors['lot-date'] = 'Это поле надо заполнить';
     }
     
-    if (isset($lot['step'])) {
-        if ($lot['step'] <= 0) {
-            $errors['step'] = 'Содержимое поля «шаг ставки» должно быть целым числом больше ноля';
+    if (isset($lot['lot-step'])) {
+        if ($lot['lot-step'] <= 0) {
+            $errors['lot-step'] = 'Содержимое поля «шаг ставки» должно быть целым числом больше ноля';
         }
     } else {
-        $errors['step'] = 'Это поле надо заполнить';
+        $errors['lot-step'] = 'Это поле надо заполнить';
     }
 
     if (count($errors)) {
@@ -79,17 +81,18 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             'categories' => $categories
         ]);
     } else {
-        move_uploaded_file($tmp_name, 'uploads/' . $path);
-        $sql = 'INSERT INTO lots (title, description, picture, price, dt_end, step, user_id, category_id) VALUES (NOW(), ?, ?, ?, ?, ?, ?, ?, ?)';
+        move_uploaded_file($_FILES['lot-img']['tmp_name'], $file_path . $file_name);
+        $sql = 'INSERT INTO lots (dt_add, user_id, category_id, title, description, picture, price, dt_end, step) VALUES (NOW(), 1, ?, ?, ?, ?, ?, ?, ?)';
 
-        $stmt = db_get_prepare_stmt($link, $sql, [$lot['category'], $lot['title'], $lot['description'], $lot['picture'], $lot['price'], $lot['dt_end'], $lot['step']]);
+        $stmt = db_get_prepare_stmt($link, $sql, [$lot['category'], $lot['lot-name'], $lot['message'], $lot['lot-img'], $lot['lot-rate'], $lot['lot-date'], $lot['lot-step']]);
         $res = mysqli_stmt_execute($stmt);
         if($res) {
             $lot_id = mysqli_insert_id($link);
             header('Location: lot.php?id=' . $lot_id);
             $page_content = include_template('lot.php', [
-                'lot' => $lot, 
+                'lot' => $lot,
             ]);
+            
         }
     }
 
--- templates/add.php
+++ templates/add.php
@@ -3,12 +3,12 @@
       <h2>Добавление лота</h2>
       <div class="form__container-two">
       <?php $classname = isset($errors['title']) ? "form__item--invalid" : "";
-      $value = isset($lot['title']) ? $lot['title'] : ""; ?>
+      $value = esc($lot['lot-name'] ?? ""); ?>
         <div class="form__item <?= $classname; ?>">
           <label for="lot-name">Наименование <sup>*</sup></label>
           <input id="lot-name" type="text" name="lot-name" placeholder="Введите наименование лота" value="<?=$value; ?>">
-          <?php if (isset($errors['title'])) : ?> 
-            <span class="form__error"><?=$errors['title']; ?></span>
+          <?php if (isset($errors['lot-name'])) : ?> 
+            <span class="form__error"><?=$errors['lot-name']; ?></span>
           <?php endif; ?>
         </div>
         <?php $classname = isset($errors['category']) ? "form__item--invalid" : ""; ?>
@@ -25,52 +25,55 @@
         <?php endif; ?>
         </div>
       </div>
-      <?php $classname = isset($errors['description']) ? "form__item--invalid" : "" ;
-      $value = isset($lot['description']) ? $lot['description'] : ""; ?>
+      <?php $classname = isset($errors['message']) ? "form__item--invalid" : "" ;
+      $value = esc($lot['message'] ?? ""); ?>
       <div class="form__item form__item--wide <?=$classname; ?>">
         <label for="message">Описание <sup>*</sup></label>
-        <textarea id="message" name="message" placeholder="Напишите описание лота" value="<?=$value;?>"></textarea>
-        <?php if (isset($errors['description'])) : ?>
-            <span class="form__error"><?=$errors['description']; ?></span>
+        <textarea id="message" name="message" placeholder="Напишите описание лота" value="<?=$value; ?>"><?=$value;?></textarea>
+        <?php if (isset($errors['message'])) : ?>
+            <span class="form__error"><?=$errors['message']; ?></span>
         <?php endif; ?>
       </div>
-      <?php $classname = isset($errors['picture']) ? "form__item--invalid" : "" ;
-      $value = isset($lot['picture']) ? $lot['picture'] : ""; ?>
+      <?php $classname = isset($errors['lot-img']) ? "form__item--invalid" : "" ;
+      $value = esc($lot['lot-img'] ?? ""); ?>
       <div class="form__item form__item--file <?=$classname;?>">
         <label>Изображение <sup>*</sup></label>
         <div class="form__input-file">
-          <input class="visually-hidden" type="file" id="lot-img" value="<?=$value;?>">
+          <input class="visually-hidden" type="file" id="lot-img" name="lot-img" value="<?=$value;?>">
           <label for="lot-img">
             Добавить
           </label>
         </div>
+        <?php if (isset($errors['lot-img'])) : ?>
+            <span class="form__error"><?=$errors['lot-img']; ?></span>
+        <?php endif; ?>
       </div>
       <div class="form__container-three">
-      <?php $classname = isset($errors['price']) ? "form__item--invalid" : "" ;
-      $value = isset($lot['price']) ? $lot['price'] : ""; ?>
+      <?php $classname = isset($errors['lot-rate']) ? "form__item--invalid" : "" ;
+      $value = esc($lot['lot-rate'] ?? ""); ?>
         <div class="form__item form__item--small <?=$classname;?>">
           <label for="lot-rate">Начальная цена <sup>*</sup></label>
           <input id="lot-rate" type="text" name="lot-rate" placeholder="0" value="<?=$value;?>">
-          <?php if (isset($errors['price'])) : ?>
-            <span class="form__error"><?=$errors['price'];?></span>
+          <?php if (isset($errors['lot-rate'])) : ?>
+            <span class="form__error"><?=$errors['lot-rate'];?></span>
           <?php endif; ?>
         </div>
-        <?php $classname = isset($errors['step']) ? "form__item--invalid" : "" ;
-        $value = isset($lot['step']) ? $lot['step'] : ""; ?>
+        <?php $classname = isset($errors['lot-step']) ? "form__item--invalid" : "" ;
+        $value = esc($lot['lot-step'] ?? ""); ?>
         <div class="form__item form__item--small <?=$classname;?>">
           <label for="lot-step">Шаг ставки <sup>*</sup></label>
           <input id="lot-step" type="text" name="lot-step" placeholder="0" value="<?=$value;?>">
-          <?php if (isset($errors['price'])) : ?>
-            <span class="form__error"><?=$errors['step'];?></span>
+          <?php if (isset($errors['lot-step'])) : ?>
+            <span class="form__error"><?=$errors['lot-step'];?></span>
           <?php endif; ?>
         </div>
-        <?php $classname = isset($errors['dt_end']) ? "form__item--invalid" : "" ;
-        $value = isset($lot['dt_end']) ? $lot['dt_end'] : ""; ?>
+        <?php $classname = isset($errors['lot-date']) ? "form__item--invalid" : "" ;
+        $value = esc($lot['lot-date'] ?? ""); ?>
         <div class="form__item <?=$classname;?>">
           <label for="lot-date">Дата окончания торгов <sup>*</sup></label>
           <input class="form__input-date" id="lot-date" type="text" name="lot-date" placeholder="Введите дату в формате ГГГГ-ММ-ДД" value="<?=$value;?>">
-          <?php if (isset($errors['dt_end'])) : ?>
-            <span class="form__error"><?=$errors['dt_end'];?></span>
+          <?php if (isset($errors['lot-date'])) : ?>
+            <span class="form__error"><?=$errors['lot-date'];?></span>
           <?php endif; ?>
         </div>
       </div>
